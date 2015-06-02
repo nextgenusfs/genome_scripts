def3e9686f4afa96b4745b945614be334b40a17f
Jon Palmer <nextgen.usfs@gmail.com>
2015-06-02 13:01:46 -0500

    Update rename_tree.pl

--- rename_tree.pl
+++ rename_tree.pl
@@ -2,6 +2,7 @@
 
 #script to rename from 2 column file that has old name tab new name
 #the new name cannot contain any of these characters or will mess up newick format ( , : ; ) 
+#script from Kenosis @ biostars https://www.biostars.org/p/76972/
 
 use strict;
 use warnings;
