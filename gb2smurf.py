465dc085a84f07d2395c60295301591b9bb037f0
Jon Palmer <nextgen.usfs@gmail.com>
2015-06-03 23:15:04 -0500

    version 3.  prints proteins, scaffolds, and SMURF annotation

--- gb2smurf.py
+++ gb2smurf.py
@@ -11,8 +11,9 @@ parser=argparse.ArgumentParser(
     description='''Script that generates SMURF input from Genbank Flat File ''',
     epilog="""Written by Jon Palmer (2015)  palmer.jona@gmail.com""")
 parser.add_argument('--gb', help='Genbank file')
-parser.add_argument('--fasta', help='Fasta output')
-parser.add_argument('--smurf', help='Smurf output')
+parser.add_argument('--prot', default="smurf.proteins.fasta", help='Fasta output')
+parser.add_argument('--dna', default="smurf.scaffolds.fasta", help='Fasta output')
+parser.add_argument('--smurf', default="smurf.annotations.txt", help='Smurf output')
 args=parser.parse_args()
 
 if len(sys.argv) < 2:
@@ -21,12 +22,18 @@ if len(sys.argv) < 2:
 
 gbk_file = args.gb
 smurf_file = args.smurf
-fasta_file = args.fasta
+fasta_file = args.prot
+contig_file = args.dna
 
 smurf_file_handle = open(smurf_file, "w")
 fasta_file_handle = open(fasta_file, "w")
+contig_file_handle = open(contig_file, "w")
 
 for record in SeqIO.parse(gbk_file, "genbank"):
+    scaffold_name = record.name[6:]
+    contig_file_handle.write(">%s\n%s\n" % (
+        scaffold_name.lstrip("0"), 
+        record.seq))
     for f in record.features:
         name = record.name[6:]
         if f.type == "CDS":
@@ -43,5 +50,6 @@ for record in SeqIO.parse(gbk_file, "genbank"):
             else:
                 smurf_file_handle.write("%s\t%s\t%s\t%s\t%s\n" % (locus_tag, name.lstrip("0"), int(myend), int(mystart), product_name))
 
-smurf_file_handle.close()
+contig_file_handle.close()
+fasta_file_handle.close()
 smurf_file_handle.close()
