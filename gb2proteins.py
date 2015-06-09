ea5c9bce81c227408477d26634f18dc37c47486d
Jon Palmer <jon@Jons-MacBook-Pro.local>
2015-06-09 16:43:16 -0500

    update script to handle JGI format

--- gb2proteins.py
+++ gb2proteins.py
@@ -7,6 +7,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('gbk', help='Genbank file')
 parser.add_argument('fasta', help='fasta file')
+parser.add_argument('--maker', action="store_true", help='Maker -T1 ending to names')
+parser.add_argument('--jgi', action="store_true", help='JGI use name instead of locus tag')
 args = parser.parse_args()
 
 gbk_filename = args.gbk
@@ -16,15 +18,24 @@ input_handle  = open(gbk_filename, "r")
 output_handle = open(faa_filename, "w")
 ending = "-T1"
 for seq_record in SeqIO.parse(input_handle, "genbank") :
-    print "Dealing with GenBank record %s" % seq_record.id
     for seq_feature in seq_record.features :
         if seq_feature.type=="CDS" :
             assert len(seq_feature.qualifiers['translation'])==1
-            output_handle.write(">%s%s\n%s\n" % (
-                   seq_feature.qualifiers['locus_tag'][0],
-                   ending,
-                   seq_feature.qualifiers['translation'][0]))
+            if args.maker:
+                output_handle.write(">%s%s\n%s\n" % (
+                    seq_feature.qualifiers['locus_tag'][0],
+                    ending,
+                    seq_feature.qualifiers['translation'][0]))
+            else:
+                if args.jgi:
+                    output_handle.write(">%s\n%s\n" % (
+                        seq_feature.qualifiers['gene'][0],
+                        seq_feature.qualifiers['translation'][0]))
+                else:
+                    output_handle.write(">%s\n%s\n" % (
+                        seq_feature.qualifiers['locus_tag'][0],
+                        seq_feature.qualifiers['translation'][0]))
 
 output_handle.close()
 input_handle.close()
-print "Done"
+print "Proteins extracted from " + gbk_filename + " saved to " + faa_filename
