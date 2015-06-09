0fa474b56b36b1d4075f62ba359263caf9e4c6a4
Jon Palmer <jon@Jons-MacBook-Pro.local>
2015-06-09 16:42:56 -0500

    update script to handle JGI format

--- gb2smurf.py
+++ gb2smurf.py
@@ -4,6 +4,7 @@
 #written by Jon Palmer palmer.jona at gmail dot com
 
 import sys
+import re
 from Bio import SeqIO
 import argparse
 
@@ -13,7 +14,9 @@ parser=argparse.ArgumentParser(
 parser.add_argument('gbk', help='Genbank file (Required)')
 parser.add_argument('-p','--prot', default="smurf.proteins.fasta", help='Fasta output')
 parser.add_argument('-g','--dna', default="smurf.scaffolds.fasta", help='Fasta output')
-parser.add_argument('s','--smurf', default="smurf.annotations.txt", help='Smurf output')
+parser.add_argument('-s','--smurf', default="smurf.annotations.txt", help='Smurf output')
+parser.add_argument('--ncbi', action='store_true', help='NCBI chromosome names')
+parser.add_argument('--jgi', action='store_true', help='JGI naming')
 args=parser.parse_args()
 
 if len(sys.argv) < 2:
@@ -30,25 +33,45 @@ fasta_file_handle = open(fasta_file, "w")
 contig_file_handle = open(contig_file, "w")
 
 for record in SeqIO.parse(gbk_file, "genbank"):
-    scaffold_name = record.name[6:]
+    if args.ncbi:
+        scaffold_name = record.name[6:]
+    else:
+        scaffold_name = re.sub('[^0-9]','', record.name)
     contig_file_handle.write(">%s\n%s\n" % (
         scaffold_name.lstrip("0"), 
         record.seq))
     for f in record.features:
-        name = record.name[6:]
+        if args.ncbi:
+            name = record.name[6:]
+        else:
+            name = re.sub('[^0-9]','', record.name)
         if f.type == "CDS":
-            fasta_file_handle.write(">%s\n%s\n" % (
+            if args.jgi:
+                fasta_file_handle.write(">%s\n%s\n" % (
+                   f.qualifiers['gene'][0],
+                   f.qualifiers['translation'][0]))
+                locus_tag = f.qualifiers.get("gene", ["No ID"])[0]
+                product_name = f.qualifiers.get("product", ["No Description"])[0]
+                mystart = f.location.start
+                myend = f.location.end
+                strand = f.location.strand
+                if strand == 1:
+                    smurf_file_handle.write("%s\t%s\t%s\t%s\t%s\n" % (locus_tag, name.lstrip("0"), int(mystart), int(myend), product_name))
+                else:
+                    smurf_file_handle.write("%s\t%s\t%s\t%s\t%s\n" % (locus_tag, name.lstrip("0"), int(myend), int(mystart), product_name))
+            else:
+                fasta_file_handle.write(">%s\n%s\n" % (
                    f.qualifiers['locus_tag'][0],
                    f.qualifiers['translation'][0]))
-            locus_tag = f.qualifiers.get("locus_tag", ["No ID"])[0]
-            product_name = f.qualifiers.get("product", ["No Description"])[0]
-            mystart = f.location.start
-            myend = f.location.end
-            strand = f.location.strand
-            if strand == 1:
-                smurf_file_handle.write("%s\t%s\t%s\t%s\t%s\n" % (locus_tag, name.lstrip("0"), int(mystart), int(myend), product_name))
-            else:
-                smurf_file_handle.write("%s\t%s\t%s\t%s\t%s\n" % (locus_tag, name.lstrip("0"), int(myend), int(mystart), product_name))
+                locus_tag = f.qualifiers.get("locus_tag", ["No ID"])[0]
+                product_name = f.qualifiers.get("product", ["No Description"])[0]
+                mystart = f.location.start
+                myend = f.location.end
+                strand = f.location.strand
+                if strand == 1:
+                    smurf_file_handle.write("%s\t%s\t%s\t%s\t%s\n" % (locus_tag, name.lstrip("0"), int(mystart), int(myend), product_name))
+                else:
+                    smurf_file_handle.write("%s\t%s\t%s\t%s\t%s\n" % (locus_tag, name.lstrip("0"), int(myend), int(mystart), product_name))
 
 contig_file_handle.close()
 fasta_file_handle.close()
