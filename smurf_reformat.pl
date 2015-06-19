792107e71901646b67688d56ffe16b8d06e5e59d
Jon Palmer <jon@Jons-MacBook-Pro.local>
2015-06-19 14:06:44 -0500

    take SMURF output and print tab delimited result with AA and DNA sequences for each hit

--- smurf_reformat.pl
+++ smurf_reformat.pl
@@ -10,7 +10,8 @@
 	smurf_reformat.pl -i Secondary-Metabolite-Clusters.txt -g genome.fasta -p proteins.fasta
 
 =head2 Options
- -i|--in
+ -c|--cluster
+ -b|--backbone
  -g|--genome
  -p|--proteins
  -v|--version
@@ -31,13 +32,16 @@ use Bio::SeqIO;
 use Bio::DB::Fasta;
 
 #set command line arguments
-my ($infi, $scaffolds, $proteins) = @ARGV;
+my ($infi, $backbone, $scaffolds, $proteins) = @ARGV;
 my $version="smurf_reformat.pl\tv0.1";
+my $out="smurf";
 my $helpAsked;
 GetOptions(
-	'i|in:s'=>\$infi,
+	'c|cluster:s'=>\$infi,
+	'b|backbone:s'=>\$backbone,
 	'g|genome:s'=>\$scaffolds,
 	'p|proteins:s'=>\$proteins, #protein fasta file
+	'o|out:s'=>\$out, #out name stem
 	'h|help:s'=>\$helpAsked,
 	'v|version'=>sub{print $version."\n"; exit;},
 );
@@ -50,12 +54,14 @@ sub prtUsage {
 print "\n";
 	print "Options:\n";
 	print "-------------------------------------------------------------\n";
-	print "-i (--in):  SMURF Cluster file \n";
+	print "-c (--cluster):  SMURF Cluster file \n";
+	print "-b (--bacbone):  SMURF Backbone file\n";
 	print "-g (--genome):  gDNA sequences fasta format\n";
 	print "-p (--proteins):  protein sequences fasta format \n";
+	print "-o (--out): output name stem (out_clusters.txt) \n";
 	print "-h (--help): Print this message\n";
 	print "-------------------------------------------------------------\n";
-	print "example:  smurf_reformat.pl -i Secondary-Metabolite-Clusters.txt -g genome.fasta -p proteins.fasta > output.txt\n\n"
+	print "example:  smurf_reformat.pl -i Secondary-Metabolite-Clusters.txt -g genome.fasta -p proteins.fasta -o outfile  \n\n"
 
 }
 
@@ -78,8 +84,10 @@ while (my $inseq2 = $seqIN2->next_seq) {
     $seqOUT2->write_seq($inseq2);
 }
 
+my $output .= "$out\_clusters.txt";
+open(OUTPUT, '>'.$output) or die;
 open my $fh, "<", $infi;
-print "#Cluster\tGene_ID\tChromosome-Scaffold\t5' end\t3' end\tDescription\tProtSeq\tDNASeq\n";
+print OUTPUT "#Cluster\tGene_ID\tChromosome-Scaffold\t5' end\t3' end\tDescription\tProtSeq\tDNASeq\n";
 my %names;
 my $count;
 while (<$fh>) {
@@ -87,19 +95,47 @@ while (<$fh>) {
     next if /^Backbone/;
     next if /^\n/;
     next if /^\r$/;
+    chomp;
     my @row = split(/\t/,$_);
     if (not exists $names{$row[0]}) {
         $names{$row[0]} = "Cluster_" . ++$count;
     }
     my $db = Bio::DB::Fasta->new('smurf.scaffolds.tmp');
     my $subseq = $db->seq($row[3], $row[5], $row[6]);
+    chomp($subseq);
     my $db2 = Bio::DB::Fasta->new('smurf.proteins.tmp');
     my $protseq = $db2->seq($row[1]);
-    print "$names{$row[0]}\t$row[1]\t$row[3]\t$row[5]\t$row[6]\t$row[9]\t$protseq\t$subseq\n";
+    chomp($protseq);
+    print OUTPUT "$names{$row[0]}\t$row[1]\t$row[3]\t$row[5]\t$row[6]\t$row[9]\t$protseq\t$subseq\n";
+
 
     }
 
 close $fh;
+
+my $output2 .= "$out\_backbone.txt";
+open(OUTPUT2, '>'.$output2) or die;
+open my $fh2, "<", $backbone;
+print OUTPUT2 "#Gene_ID\tPrediction\tChromosome-Scaffold\t5' end\t3' end\tDescription\tProtSeq\tDNASeq\n";
+while (<$fh2>) {
+    next if /^Backbone/;
+    next if /^\n/;
+    next if /^\r$/;
+    chomp;
+    my @field = split(/\t/,$_);
+    my $db3 = Bio::DB::Fasta->new('smurf.scaffolds.tmp');
+    my $subseq2 = $db3->seq($field[2], $field[4], $field[5]);
+    chomp($subseq2);
+    my $db4 = Bio::DB::Fasta->new('smurf.proteins.tmp');
+    my $protseq2 = $db4->seq($field[0]);
+    chomp($protseq2);
+    print OUTPUT2 "$field[0]\t$field[6]\t$field[2]\t$field[4]\t$field[5]\t$field[1]\t$protseq2\t$subseq2\n";
+
+
+    }
+close $fh2;
+
+
 #clean up temp files
 unlink 'smurf.scaffolds.tmp';
 unlink 'smurf.scaffolds.tmp.index';
