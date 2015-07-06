b4f13da2f554e777071de5b94dd85fb7cf0ee3f7
Ion PGM <ngs@ngs-mac-pro.fpl.fs.fed.us>
2015-07-06 07:02:55 -0500

    updated script to include length filtering

--- sort_rename_fasta.py
+++ sort_rename_fasta.py
@@ -14,6 +14,7 @@ parser=argparse.ArgumentParser(
 parser.add_argument('fasta', help='Fasta file (Required)')
 parser.add_argument('-o','--out', default="sort.renamed.fasta", help='Fasta output')
 parser.add_argument('-b','--base', default="Supercontig", help='Base name for fasta headers')
+parser.add_argument('-l','--length', default="1", help='min length to keep seq')
 args=parser.parse_args()
 
 if len(sys.argv) < 2:
@@ -22,14 +23,25 @@ if len(sys.argv) < 2:
 
 fasta_in = args.fasta
 base_name = args.base
+length = int(args.length)
+
+#get rid of sequences shorter than min length
+seq_pass = []
+for record in SeqIO.parse(fasta_in, "fasta"):
+    if len(record.seq) > length :
+        seq_pass.append(record)
+print "Found %i sequences" % len(seq_pass)
+tempout = open("fasta.tmp", "w")
+SeqIO.write(seq_pass, tempout, "fasta")
+
 
 #sort records and write temp file
-records = list(SeqIO.parse(fasta_in,"fasta"))
+records = list(SeqIO.parse("fasta.tmp", "fasta"))
 records.sort(cmp=lambda x,y: cmp(len(y),len(x)))
-SeqIO.write(records, "fasta.tmp", "fasta")
+SeqIO.write(records, "fasta.tmp2", "fasta")
 
 #load temp fasta back in and change names, prob unnecessary but couldn't figure out how to do with SeqIO
-fasta_tmp = open("fasta.tmp")
+fasta_tmp = open("fasta.tmp2")
 fasta_out = open(args.out, 'wb')
 count = 1
 for line in fasta_tmp.readlines():
@@ -39,7 +51,8 @@ for line in fasta_tmp.readlines():
 		count += 1
 	else:
 		fasta_out.write(line)
-
+tempout.close()
 fasta_out.close()
 fasta_tmp.close()
 os.remove("fasta.tmp")
+os.remove("fasta.tmp2")
