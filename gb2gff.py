5e72f44c3959a2b6cb2532a607deb8482bb184f9
Ion PGM <ngs@ngs-mac-pro.fpl.fs.fed.us>
2016-03-21 15:12:31 -0500

    updates

--- gb2gff.py
+++ gb2gff.py
@@ -1,31 +1,65 @@
 #!/usr/bin/env python
 
-#This script converts genbank format into GFF Format
-#requires BCBio gff parser, install: pip install bcbio-gff
-#script keeps stupid genbank ridiculously long chromosome/scaffold names
-
-import sys
 from Bio import SeqIO
-from BCBio import GFF
-import argparse
+import sys
 
-parser=argparse.ArgumentParser(
-    description='''Script that converts genbank to GFF3 format ''',
-    epilog="""Written by Jon Palmer (2015)  palmer.jona@gmail.com""")
-parser.add_argument('gbk', help='Genbank file')
-parser.add_argument('-g','--gff', default="gb2gff.gff3", help='GFF3 output')
-args=parser.parse_args()
+with open(sys.argv[1], 'rU') as gbk:
+    for record in SeqIO.parse(gbk, 'genbank'):
+        for f in record.features:
+            if f.type == 'CDS':
+                chr = record.id
+                ID = f.qualifiers['locus_tag'][0]
+                product = f.qualifiers['product'][0]
+                start = f.location.nofuzzy_start + 1
+                end = f.location.nofuzzy_end + 1
+                strand = f.location.strand
+                if strand == 1:
+                    strand = '+'
+                elif strand == -1:
+                    strand = '-'
+                num_exons = len(f.sub_features)
+                current_phase = 0
+                sys.stdout.write("%s\tGenBank\tgene\t%s\t%s\t.\t%s\t.\tID=%s\n" % (chr, start, end, strand, ID))
+                sys.stdout.write("%s\tGenBank\tmRNA\t%s\t%s\t.\t%s\t.\tID=%s-T1;Parent=%s;product=%s\n" % (chr, start, end, strand, ID, ID, product))
+                if num_exons < 1: #only a single exon
+                    ex_start = str(f.location.nofuzzy_start + 1)
+                    ex_end = str(f.location.nofuzzy_end + 1)
+                    sys.stdout.write("%s\tGenBank\texon\t%s\t%s\t.\t%s\t.\tID=%s-T1.exon1;Parent=%s-T1\n" % (chr, ex_start, ex_end, strand, ID, ID))
+                    sys.stdout.write("%s\tGenBank\tCDS\t%s\t%s\t.\t%s\t0\tID=%s-T1.cds;Parent=%s-T1\n" % (chr, ex_start, ex_end, strand, ID, ID))
+                else: #more than 1 exon, so parts sub_features
+                    if f.location.strand == 1:
+                        for i in range(0,num_exons):
+                            ex_start = str(f.sub_features[i].location.nofuzzy_start + 1)
+                            ex_end = str(f.sub_features[i].location.nofuzzy_end + 1)
+                            ex_num = i + 1
+                            sys.stdout.write("%s\tGenBank\texon\t%s\t%s\t.\t%s\t.\tID=%s-T1.exon%i;Parent=%s-T1\n" % (chr, ex_start, ex_end, strand, ID, ex_num, ID))
+                            sys.stdout.write("%s\tGenBank\tCDS\t%s\t%s\t.\t%s\t%i\tID=%s-T1.cds;Parent=%s-T1\n" % (chr, ex_start, ex_end, strand, current_phase, ID, ID))
+                            current_phase = 3 - (((int(ex_end) - int(ex_start)) - current_phase) % 3)
+                            if current_phase == 3:
+                                current_phase = 0
+                    else:
+                        for i in reversed(range(0,num_exons)):
+                            phase = 0
+                            ex_start = str(f.sub_features[i].location.nofuzzy_start + 1)
+                            ex_end = str(f.sub_features[i].location.nofuzzy_end + 1)
+                            ex_num = num_exons - i
+                            sys.stdout.write("%s\tGenBank\texon\t%s\t%s\t.\t%s\t.\tID=%s-T1.exon%i;Parent=%s-T1\n" % (chr, ex_start, ex_end, strand, ID, ex_num, ID))
+                            sys.stdout.write("%s\tGenBank\tCDS\t%s\t%s\t.\t%s\t%i\tID=%s-T1.cds;Parent=%s-T1\n" % (chr, ex_start, ex_end, strand, current_phase, ID, ID))
+                            current_phase = 3 - (((int(ex_end) - int(ex_start)) - current_phase) % 3)
+                            if current_phase == 3:
+                                current_phase = 0
 
-if len(sys.argv) < 2:
-    parser.print_usage()
-    sys.exit(1)
- 
-in_file = args.gbk
-out_file = args.gff
-in_handle = open(in_file)
-out_handle = open(out_file, "w")
- 
-GFF.write(SeqIO.parse(in_handle, "genbank"), out_handle)
- 
-in_handle.close()
-out_handle.close()
+            if f.type == 'tRNA':
+                ID = f.qualifiers['locus_tag'][0]
+                start = f.location.nofuzzy_start
+                end = f.location.nofuzzy_end
+                strand = f.location.strand
+                if strand == 1:
+                    strand = '+'
+                elif strand == -1:
+                    strand = '-'
+                product = f.qualifiers['product'][0]
+                chr = record.id
+                sys.stdout.write("%s\tGenBank\tgene\t%s\t%s\t.\t%s\t.\tID=%s\n" % (chr, start, end, strand, ID))
+                sys.stdout.write("%s\tGenBank\ttRNA\t%s\t%s\t.\t%s\t.\tID=%s-T1;Parent=%s;product=%s\n" % (chr, start, end, strand, ID, ID, product))
+                sys.stdout.write("%s\tGenBank\texon\t%s\t%s\t.\t%s\t.\tID=%s-T1.exon1;Parent=%s-T1\n" % (chr, start, end, strand, ID, ID, product))
--- proteome_compare.py
+++ proteome_compare.py
@@ -6,41 +6,68 @@ from natsort import natsorted
 #script to run global alignment using usearch8 and then parse the output
 
 if len(sys.argv) < 2:
-    print "Usage: proteome_compare.py query.faa reference.faa"
+    print "Usage: proteome_compare.py query.faa reference.faa singletons.output.txt okay.hits.txt"
     sys.exit(1)
 
 def compare(input):
+    global singletons
+    global good
+    singletons = []
+    good = []
+    cantsithereseatstaken = []
     with open(input, 'rU') as input:
         identical = 0; great = 0; okay = 0; partial = 0; unique = 0; others = 0; bad = 0;
         for line in input:
             col = line.split('\t')
             if col[1] == '*':
+                singletons.append([col[0]])
                 unique += 1
                 continue
             if float(col[2]) == 100.0 and int(col[3]) == int(col[4]) and int(col[5]) == int(col[6]):
                 identical += 1
+                if not col[1] in cantsithereseatstaken:
+                    good.append([col[0],col[1]])
+                    cantsithereseatstaken.append(col[1])
                 continue
             elif float(col[2]) >= 95.0 and int(col[5]) >= 95 and int(col[6]) >= 95:
                 great += 1
+                if not col[1] in cantsithereseatstaken:
+                    good.append([col[0],col[1]])
+                    cantsithereseatstaken.append(col[1])
                 continue
             elif float(col[2]) >= 80.0 and int(col[5]) >= 80 and int(col[6]) >= 80:
                 okay += 1
+                if not col[1] in cantsithereseatstaken:
+                    good.append([col[0],col[1]])
+                    cantsithereseatstaken.append(col[1])
                 continue
             elif float(col[2]) >= 60.0 and int(col[5]) >= 60 and int(col[6]) >= 60:
                 others +=1
+                if not col[1] in cantsithereseatstaken:
+                    good.append([col[0],col[1]])
+                    cantsithereseatstaken.append(col[1])
                 continue
             else:
                 bad += 1
+                singletons.append([col[0]])
     return (identical, great, okay, others, unique, bad)
 
-subprocess.call(['usearch8', '-usearch_global', sys.argv[1], '-db', sys.argv[2], '-id', '0.6', '-userout', 'prot_compare1.txt', '-userfields', 'query+target+id+ql+tl+qcov+tcov', '-output_no_hits', '-top_hit_only'])
+subprocess.call(['usearch8', '-usearch_global', sys.argv[1], '-db', sys.argv[2], '-id', '0.6', '-userout', 'prot_compare1.txt', '-userfields', 'query+target+id+ql+tl+qcov+tcov', '-output_no_hits', '-top_hit_only', '-notrunclabels'])
 
-subprocess.call(['usearch8', '-db', sys.argv[1], '-usearch_global', sys.argv[2], '-id', '0.6', '-userout', 'prot_compare2.txt', '-userfields', 'query+target+id+ql+tl+qcov+tcov', '-output_no_hits', '-top_hit_only'])
+subprocess.call(['usearch8', '-db', sys.argv[1], '-usearch_global', sys.argv[2], '-id', '0.6', '-userout', 'prot_compare2.txt', '-userfields', 'query+target+id+ql+tl+qcov+tcov', '-output_no_hits', '-top_hit_only', '-notrunclabels'])
 
 #parse output
 qvsr = compare('prot_compare1.txt')
-rvsq = compare('prot_compare2.txt')
 
+with open(sys.argv[3], 'w') as output:
+    for hit in singletons:
+        output.write("%s\n" % (hit[0]))
+
+with open(sys.argv[4], 'w') as output:
+    for hit in good:
+        output.write("%s\t%s\n" % (hit[0], hit[1]))
+
+rvsq = compare('prot_compare2.txt')
 
 print"--------------------------------"
 print"Query vs Ref:"
