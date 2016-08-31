19260647d0ac08fd7f47703bb3558c2426af74b8
Jon Palmer <jon@Jons-MacBook-Pro.local>
2016-08-30 22:37:43 -0500

    script to exhastive search GBK file for HMM models

--- findHMM.py
+++ findHMM.py
@@ -2,6 +2,8 @@
 
 import sys, os, subprocess, argparse, shutil, warnings, csv, multiprocessing
 from Bio import SeqIO
+from natsort import natsorted
+import pandas as pd
 with warnings.catch_warnings():
     warnings.simplefilter('ignore')
     from Bio import SearchIO
@@ -14,9 +16,9 @@ parser=argparse.ArgumentParser(prog='findHMM.py',
     description='''Find HMM model in GenBank flatfile genome.''',
     epilog="""Written by Jon Palmer (2016) nextgenusfs@gmail.com""",
     formatter_class = MyFormatter)
-parser.add_argument('-i','--input', required=True, help='Genome (GBK) format')
+parser.add_argument('-i','--input', nargs='+', required=True, help='Genome (GBK) format')
+parser.add_argument('-o','--out', default='findHMM.table.csv', help='Summary output CSV file')
 parser.add_argument('-m','--hmm', required=True, help='HMM file')
-parser.add_argument('-o','--out', required=True, help='Protein FASTA output')
 parser.add_argument('--evalue', default='1e-50', help='HMM evalue')
 parser.add_argument('--cpus', default=2, type=int, help='Number of CPUs')
 parser.add_argument('--debug', action='store_true', help='Debug')
@@ -89,129 +91,165 @@ def runExonerate(input):
         subprocess.call(['exonerate', '--model', 'p2g', '--showvulgar', 'no', '--showalignment', 'no', '--showquerygff', 'no', '--showtargetgff', 'no', '--maxintron', args.maxIntron, '--percent', '25', '--ryo', ryo , query, scaffold], stdout = output4)
     os.remove(query)
 
-
 #set up tmpdir
 tmpdir = 'tmp_'+str(os.getpid())
 os.makedirs(tmpdir)
 
-#Split GBK into parts
-base = args.input.rsplit('.', 1)[0]
-if '/' in base:
-    base = base.split('/') [-1]
-Proteins = os.path.join(tmpdir, base+'.proteins.fa')
-Transcripts = os.path.join(tmpdir, base+'.transcripts.fa')
-Genome = os.path.join(tmpdir, base+'.genome.fa')
-gb2output(args.input, Proteins, Transcripts, Genome)
+#setup dictionary to hold summary results
+AllResults = []
+labels = []
 
-#load proteins into dictionary
-protein_dict = SeqIO.to_dict(SeqIO.parse(Proteins, 'fasta'))
+for file in args.input:
+    #Split GBK into parts
+    base = file.rsplit('.', 1)[0]
+    if '/' in base:
+        base = base.split('/') [-1]
+    labels.append(base)
+    FinalOut = base+'.findHMM.fasta'
+    Proteins = os.path.join(tmpdir, base+'.proteins.fa')
+    Transcripts = os.path.join(tmpdir, base+'.transcripts.fa')
+    Genome = os.path.join(tmpdir, base+'.genome.fa')
+    gb2output(file, Proteins, Transcripts, Genome)
+    
+    #print status
+    print '----------------------------------------------'
+    print 'Working on %s' % base
+    
+    #load proteins into dictionary
+    protein_dict = SeqIO.to_dict(SeqIO.parse(Proteins, 'fasta'))
 
-#check number of HMMer models
-HMMstat = os.path.join(tmpdir, 'hmmstat.txt')
-with open(HMMstat, 'w') as output:
-    subprocess.call(['hmmstat', args.hmm], stdout = output, stderr = FNULL)
-HMMmodels = []
-with open(HMMstat, 'rU') as input:
-    for line in input:
-        if line.startswith('\n'):
-            continue
-        if not line.startswith('#'):
-            cols = line.split(' ')
-            cols = filter(None, cols)
-            if not cols[1] in HMMmodels:
-                HMMmodels.append(cols[1])
-print "Looking for %i protein HMM model(s)" % len(HMMmodels)
-#do hmmer search of proteins
-print "Scanning proteome using HMMsearch"
-HMM = os.path.join(tmpdir, base+'.hmmsearch.txt')
-subprocess.call(['hmmsearch', '-o', HMM, '--cpu', str(args.cpus), '-E', args.evalue, args.hmm, Proteins], stdout = FNULL, stderr = FNULL)
-Results = {}
-with open(HMM, 'rU') as results:
-    for qresult in SearchIO.parse(results, "hmmer3-text"):
-        query_length = qresult.seq_len #length of HMM model
-        hits = qresult.hits
-        num_hits = len(hits)
-        if num_hits > 0:
-            query = hits[0].id
-            hit = hits[0].query_id
-            score = hits[0].bitscore
-            evalue = hits[0].evalue
-            num_hsps = len(hits[0].hsps)
-            aln_length = 0
-            for x in range(0,num_hsps):
-                aln_length += hits[0].hsps[x].aln_span
-            if hit not in Results:
-                Results[hit] = (query, score, evalue, aln_length)
- 
-with open(args.out, 'w') as output:
-    for k,v in Results.items():
-        description = k+"|"+v[0]+"|evalue="+str(v[2])+"|HMMer3-Complete"
-        rec = protein_dict[v[0]]
-        rec.id = description
-        rec.description = ''
-        rec.name = ''
-        SeqIO.write(rec, output, 'fasta')
+    #check number of HMMer models
+    HMMstat = os.path.join(tmpdir, 'hmmstat.txt')
+    if not os.path.isfile(HMMstat):
+        with open(HMMstat, 'w') as output:
+            subprocess.call(['hmmstat', args.hmm], stdout = output, stderr = FNULL)
+        HMMmodels = []
+        with open(HMMstat, 'rU') as input:
+            for line in input:
+                if line.startswith('\n'):
+                    continue
+                if not line.startswith('#'):
+                    cols = line.split(' ')
+                    cols = filter(None, cols)
+                    if not cols[1] in HMMmodels:
+                        HMMmodels.append(cols[1])
+    print "Looking for %i protein HMM model(s)" % len(HMMmodels)
+    #do hmmer search of proteins
+    print "Scanning proteome using HMMsearch"
+    HMM = os.path.join(tmpdir, base+'.hmmsearch.txt')
+    subprocess.call(['hmmsearch', '-o', HMM, '--cpu', str(args.cpus), '-E', args.evalue, args.hmm, Proteins], stdout = FNULL, stderr = FNULL)
+    Results = {}
+    with open(HMM, 'rU') as results:
+        for qresult in SearchIO.parse(results, "hmmer3-text"):
+            query_length = qresult.seq_len #length of HMM model
+            hits = qresult.hits
+            num_hits = len(hits)
+            if num_hits > 0:
+                query = hits[0].id
+                hit = hits[0].query_id
+                score = hits[0].bitscore
+                evalue = hits[0].evalue
+                num_hsps = len(hits[0].hsps)
+                aln_length = 0
+                for x in range(0,num_hsps):
+                    aln_length += hits[0].hsps[x].aln_span
+                if hit not in Results:
+                    Results[hit] = (query, score, evalue, aln_length, 'Hmmer3')
+   
+    with open(FinalOut, 'w') as output:
+        for k,v in Results.items():
+            description = k+"|"+v[0]+"|evalue="+str(v[2])+"|HMMer3-Complete"
+            rec = protein_dict[v[0]]
+            rec.id = description
+            rec.description = ''
+            rec.name = ''
+            SeqIO.write(rec, output, 'fasta')
         
-notfound = []
-for i in HMMmodels:
-    if not i in Results:
-        notfound.append(i)
+    notfound = []
+    for i in HMMmodels:
+        if not i in Results:
+            notfound.append(i)
 
-if len(notfound) > 0: #have to do some more work here for these to be sure they really don't exist
-    #get consensus from hmm model
-    print "%i missing models [%s]" % (len(notfound), ', '.join(notfound))
-    Consensus = os.path.join(tmpdir, 'missing.consensi.tmp')
-    Consensi = os.path.join(tmpdir, 'missing.consensi.fa')
-    with open(Consensus, 'w') as output1:
-        subprocess.call(['hmmemit', '-c', args.hmm], stdout = output1, stderr = FNULL)
-    with open(Consensi, 'w') as output2:
-        with open(Consensus, 'rU') as input:
-            for rec in SeqIO.parse(input, 'fasta'):
-                rec.id = rec.id.replace('-consensus', '')
-                rec.name = ''
-                rec.description = ''
-                if rec.id in notfound:
-                    SeqIO.write(rec, output2, 'fasta')
+    if len(notfound) > 0: #have to do some more work here for these to be sure they really don't exist
+        #get consensus from hmm model
+        print "%i missing models [%s]" % (len(notfound), ', '.join(notfound))
+        Consensus = os.path.join(tmpdir, 'missing.consensi.tmp')
+        Consensi = os.path.join(tmpdir, 'missing.consensi.fa')
+        with open(Consensus, 'w') as output1:
+            subprocess.call(['hmmemit', '-c', args.hmm], stdout = output1, stderr = FNULL)
+        with open(Consensi, 'w') as output2:
+            with open(Consensus, 'rU') as input:
+                for rec in SeqIO.parse(input, 'fasta'):
+                    rec.id = rec.id.replace('-consensus', '')
+                    rec.name = ''
+                    rec.description = ''
+                    if rec.id in notfound:
+                        SeqIO.write(rec, output2, 'fasta')
 
-    #now run tblastn against genome with those notfound
-    Blast = os.path.join(tmpdir, 'tblastn.blast.tab')
-    print "Try to recover models using tBlastn/Exonerate"
-    tblastnFilter(Genome, Consensi, args.cpus, tmpdir)
-    print "found %i preliminary tBlastn alignments" % (len(HitList))
+        #now run tblastn against genome with those notfound
+        Blast = os.path.join(tmpdir, 'tblastn.blast.tab')
+        print "Try to recover models using tBlastn/Exonerate"
+        tblastnFilter(Genome, Consensi, args.cpus, tmpdir)
+        print "found %i preliminary tBlastn alignments" % (len(HitList))
+        if len(HitList) != 0:          
+            #split genome fasta into individual scaffolds
+            with open(Genome, 'rU') as input:
+                for record in SeqIO.parse(input, "fasta"):
+                    SeqIO.write(record, os.path.join(tmpdir, record.id + ".fa"), "fasta")
 
-    #split genome fasta into individual scaffolds
-    with open(Genome, 'rU') as input:
-        for record in SeqIO.parse(input, "fasta"):
-            SeqIO.write(record, os.path.join(tmpdir, record.id + ".fa"), "fasta")
+            #Now run exonerate on hits
+            print "Polishing alignments with Exonerate"
+            record_dict = SeqIO.to_dict(SeqIO.parse(Consensi, 'fasta'))
+            p = multiprocessing.Pool(args.cpus)
+            rs = p.map_async(runExonerate, HitList)
+            p.close()
+            while (True):
+                if (rs.ready()): break
 
-    #Now run exonerate on hits
-    print "Polishing alignments with Exonerate"
-    record_dict = SeqIO.to_dict(SeqIO.parse(Consensi, 'fasta'))
-    p = multiprocessing.Pool(args.cpus)
-    rs = p.map_async(runExonerate, HitList)
-    p.close()
-    while (True):
-        if (rs.ready()): break
-
-    #now collect all exonerate results into one
-    print "Saving all hits to %s" % args.out
-    Exonerate = os.path.join(tmpdir, 'exonerate.output.txt')
-    skip = ['Command line', '%', ' ','tmp_', '\n', '--', 'Hostname']
-    with open(Exonerate, 'w') as output5:
-        for root, dirs, files in os.walk(tmpdir):
-            for file in files:
-                if file.endswith('.out'):
-                    filename = os.path.join(root, file)
-                    with open(filename, 'rU') as readfile:
-                        for line in group_by_heading(readfile):
-                            for i in line:
-                                if not any(i.startswith(x) for x in skip):
-                                    output5.write(i)
-    with open(args.out, 'ab') as output6:
-        with open(Exonerate, 'rU') as input:
-            for rec in SeqIO.parse(input, 'fasta'):
-                output6.write('>%s\n%s\n' % (rec.id, rec.seq.translate()))
+            #now collect all exonerate results into one
+            print "Saving all hits to %s" % FinalOut
+            Exonerate = os.path.join(tmpdir, 'exonerate.output.txt')
+            skip = ['Command line', '%', ' ','tmp_', '\n', '--', 'Hostname']
+            with open(Exonerate, 'w') as output5:
+                for root, dirs, files in os.walk(tmpdir):
+                    for file in files:
+                        if file.endswith('.out'):
+                            filename = os.path.join(root, file)
+                            with open(filename, 'rU') as readfile:
+                                for line in group_by_heading(readfile):
+                                    for i in line:
+                                        if not any(i.startswith(x) for x in skip):
+                                            output5.write(i)
+        
+            with open(FinalOut, 'ab') as output6:
+                with open(Exonerate, 'rU') as input:
+                    for rec in SeqIO.parse(input, 'fasta'):
+                        info = rec.id.split('|')
+                        if not info[0] in Results:
+                            Results[info[0]] = (info[2], 'NA', info[1], 'NA', 'Exonerate')
+                        output6.write('>%s\n%s\n' % (rec.id, rec.seq.translate()))
+        else:
+            print "No potential hits found"
             
+        for i in HMMmodels:
+            if not i in Results:
+                Results[i] = ('None found', 'NA', 'NA', 'NA', 'NA')
+    else:
+        print "Saving all hits to %s" % FinalOut
+        
+    TextOut = base+'.findHMM.txt'
+    with open(TextOut, 'w') as output:
+        output.write('HMM-Model\tHit\tBitScore\tEvalue\tAlign-Len\tMethod\n')
+        for k,v in natsorted(Results.items()):
+            output.write('%s\t%s\t%s\t%s\t%s\t%s\n' % (k, v[0], v[1], v[2], v[3], v[4]))
+    SumResults = {}
+    for k,v in Results.items():
+        SumResults[k] = v[0]            
+    AllResults.append(SumResults)
 
+print '----------------------------------------------'
+df = pd.DataFrame(AllResults, index=labels)
+df.to_csv(args.out)
+print 'Summary table saved to %s' % args.out
 if not args.debug:
     shutil.rmtree(tmpdir)
