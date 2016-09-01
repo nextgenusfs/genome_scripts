d1ad805cc3c8b443a44636e3f08e9696160d6459
Ion PGM <ngs@ngs-mac-pro.fpl.fs.fed.us>
2016-09-01 14:09:40 -0500

    scripts to generate UTAX trainable datasets

--- bold2utax.py
+++ bold2utax.py
@@ -1,15 +1,104 @@
 #!/usr/bin/env python
 
-import sys, re
+import sys, re, argparse
 from natsort import natsorted
 
-if len(sys.argv) < 2:
-    print("Purpose: Script will parse Bold DB txt output to fasta file, keeping only records deposited in GenBank\nUsage: bold2utax.py bold_data.txt > formatted.fa")
-    sys.exit(1)
+#setup menu with argparse
+class MyFormatter(argparse.ArgumentDefaultsHelpFormatter):
+    def __init__(self,prog):
+        super(MyFormatter,self).__init__(prog,max_help_position=48)
+parser=argparse.ArgumentParser(prog='bold2utax.py',
+    description='''Parse BOLD DB TSV data dump into FASTA with UTAX compatible labels.''',
+    epilog="""Written by Jon Palmer (2016) nextgenusfs@gmail.com""",
+    formatter_class = MyFormatter)
+parser.add_argument('-i','--input', required=True, help='Bold data dump TSV format')
+parser.add_argument('-o','--out', required=True, help='UTAX formated FASTA output')
+parser.add_argument('--primer', default='GGTCAACAAATCATAAAGATATTGG', help='Forward Primer Sequence')
+parser.add_argument('--trim_length', default='250', help='Distance to Trim sequences to')
+parser.add_argument('--primer_mismatch', default='4', help='Mismatches allowed in primer')
+parser.add_argument('--require_genbank', action='store_true', help='Require output to have GenBank Accessions')
+args=parser.parse_args()
+
+LetterToSet = {}
+LetterToSet['A'] = "A"
+LetterToSet['C'] = "C"
+LetterToSet['G'] = "G"
+LetterToSet['T'] = "T"
+LetterToSet['M'] = "AC"
+LetterToSet['R'] = "AG"
+LetterToSet['W'] = "AT"
+LetterToSet['S'] = "CG"
+LetterToSet['Y'] = "CT"
+LetterToSet['K'] = "GT"
+LetterToSet['V'] = "ACG"
+LetterToSet['H'] = "ACT"
+LetterToSet['D'] = "AGT"
+LetterToSet['B'] = "CGT"
+LetterToSet['X'] = "GATC"
+LetterToSet['N'] = "GATC"
+
+def MatchLetter(a, b):
+	global LetterToSet
+	try:
+		sa = LetterToSet[a.upper()]
+	except:
+		return False
+	try:
+		sb = LetterToSet[b.upper()]
+	except:
+		return False
+	for ca in sa:
+		if ca in sb:
+			return True
+	return False
+
+def MatchPrefix(Seq, Primer):
+	L = len(Seq)
+	PrimerLength = len(Primer)
+	n = PrimerLength
+	if L < n:
+		n = L
+	Diffs = 0
+	for i in range(0, n):
+		if not MatchLetter(Seq[i], Primer[i]):
+			Diffs += 1
+	return Diffs
+
+def BestMatch(Seq, Primer):
+	L = len(Seq)
+	PrimerLength = len(Primer)
+	BestDiffs = PrimerLength
+	BestPos = -1
+	for Pos in range(0, L-PrimerLength+1):
+		d = MatchPrefix(Seq[Pos:], Primer)
+		if d < BestDiffs:
+			BestDiffs = d
+			BestPos = Pos
+	return BestPos, BestDiffs
+
+def TrimPrimer(Sequence, primer, trimlen, mismatch):
+    #find primer location
+    Diffs = BestMatch(Sequence, primer)
+    if Diffs[1] > int(mismatch):
+        #assume that primer was trimmed off
+        Seq = Sequence[:int(trimlen)]
+    else:
+        Seq = Sequence[Diffs[0]+len(primer):int(trimlen)]
+    #do a length check, if shorter than 250 bp, discard
+    if len(Seq) >= int(trimlen):
+        return Seq
+
+Total = -1
+NoPrimer = 0
+PrimerFound = 0
+TooShort = 0
+UniqueBIN = 0
+DupBIN = 0
 
 bins_seen = {}
-with open(sys.argv[1], 'rU') as input:
+with open(args.input, 'rU') as input:
     for line in input:
+        Total += 1
         line = line.replace('\n', '')
         if line.startswith('processid'):
             header = line.split('\t')
@@ -26,7 +115,7 @@ with open(sys.argv[1], 'rU') as input:
             idby = header.index('identification_provided_by')
             continue
         col = line.split('\t')
-        #some idiots have collector names in these places in the DB, this DB is kind of a mess....
+        #some idiots have collector names in these places in the DB, this DB is kind of a mess, doing the best I can....
         bd = col[idby].strip()
         badnames = bd.split(' ')
         badfiltered = ['1','2','3','4','5','6','7','8','9','0']
@@ -53,29 +142,52 @@ with open(sys.argv[1], 'rU') as input:
                 S = ''
         ID = col[boldid].strip()
         GB = col[gbid].strip()
-        if GB: #if there is a GB accession
-            if 'Pending' in GB:
-                continue
-            Seq = col[seqid].replace('-', '')
-            Seq = re.sub('N*$', '', Seq)
-            Seq = re.sub('^N*', '', Seq)
-            tax = []
-            for i in [K,P,C,O,F,G,S]:
-                if not i.endswith(':'):
-                    tax.append(i)
-            tax_fmt = ','.join(tax)
-            if tax_fmt.endswith(',') or tax_fmt.endswith(', '):
-                tax_fmt = tax_fmt.rsplit(',',1)[0]
-            BIN = col[bin].strip()
-            if BIN == '':
-                continue
-            if not BIN in bins_seen:
-                bins_seen[BIN] = (tax_fmt, GB, Seq)
+        if args.require_genbank:
+            if GB: #if there is a GB accession
+                if 'Pending' in GB:
+                    continue
+                else:
+                    pass
             else:
-                oldtax = bins_seen.get(BIN)[0]
-                oldlevels = oldtax.count(',')
-                if tax_fmt.count(',') > oldlevels:
-                    bins_seen[BIN] = (tax_fmt, GB, Seq)
+                continue
+        Seq = col[seqid].replace('-', '')
+        Seq = re.sub('N*$', '', Seq)
+        Seq = re.sub('^N*', '', Seq)
+        tax = []
+        for i in [K,P,C,O,F,G,S]:
+            if not i.endswith(':'):
+                tax.append(i)
+        tax_fmt = ','.join(tax)
+        if tax_fmt.endswith(',') or tax_fmt.endswith(', '):
+            tax_fmt = tax_fmt.rsplit(',',1)[0]
+        BIN = col[bin].strip()
+        if BIN == '':
+            continue
+        if not BIN in bins_seen:
+            TrimSeq = TrimPrimer(Seq, args.primer, args.trim_length, args.primer_mismatch)
+            if TrimSeq:
+                bins_seen[BIN] = (tax_fmt, GB, TrimSeq)
+                UniqueBIN += 1
+            else:
+                TooShort +=1
+        else:
+            DupBIN += 1
+            oldtax = bins_seen.get(BIN)[0]
+            oldlevels = oldtax.count(',')
+            if tax_fmt.count(',') > oldlevels:
+                TrimSeq = TrimPrimer(Seq, args.primer, args.trim_length, args.primer_mismatch)
+                if TrimSeq:
+                    bins_seen[BIN] = (tax_fmt, GB, TrimSeq)
 
-for k,v in natsorted(bins_seen.items()):
-    sys.stdout.write('>%s_%s;tax=%s\n%s\n' % (k, v[1], v[0], v[2]))
+with open(args.out, 'w') as output:
+    for k,v in natsorted(bins_seen.items()):
+        if v[1] != '':
+            output.write('>%s_%s;tax=%s\n%s\n' % (k, v[1], v[0], v[2]))
+        else:
+            output.write('>%s;tax=%s\n%s\n' % (k, v[0], v[2]))
+
+print "%i total records processed" % Total
+print "%i records duplicate BIN" % DupBIN
+print "%i seq too short (%s bp)" % (TooShort, args.trim_length)
+print "%i records written to %s" % (UniqueBIN, args.out)
+
---
+++ cluster2utax.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+import sys, os, argparse, subprocess, shutil
+from Bio import SeqIO
+from natsort import natsorted
+
+#setup menu with argparse
+class MyFormatter(argparse.ArgumentDefaultsHelpFormatter):
+    def __init__(self,prog):
+        super(MyFormatter,self).__init__(prog,max_help_position=48)
+parser=argparse.ArgumentParser(prog='cluster2utax.py',
+    description='''Take output from bold2utax and cluster to 97 pct, then use LCA to rename taxonomy.''',
+    epilog="""Written by Jon Palmer (2016) nextgenusfs@gmail.com""",
+    formatter_class = MyFormatter)
+parser.add_argument('-i','--input', required=True, help='FASTA file from bold2utax.py')
+parser.add_argument('-o','--out', required=True, help='Output basename')
+parser.add_argument('--debug', action='store_true', help='Keep intermediate files')
+args=parser.parse_args()
+
+def countfasta(input):
+    count = 0
+    with open(input, 'rU') as f:
+        for line in f:
+            if line.startswith (">"):
+                count += 1
+    return count
+
+#set up tmpdir
+tmpdir = 'tmp_'+str(os.getpid())
+os.makedirs(tmpdir)
+print "----------------------------------"
+#run dereplicatoin using vsearch
+print "Dereplication"
+print "----------------------------------"
+Derep = os.path.join(tmpdir, 'derep.fa')
+subprocess.call(['vsearch', '--derep_fulllength', args.input, '--sizeout', '--output', Derep, '--notrunclabels'])
+print "----------------------------------"
+
+#now cluster at 97%
+print "Clustering"
+print "----------------------------------"
+OTUs = os.path.join(tmpdir, 'otus.fa')
+UC = os.path.join(tmpdir, 'otus.uc')
+subprocess.call(['vsearch', '--cluster_size', Derep, '--id', '0.97', '--centroids', OTUs, '--uc', UC, '--notrunclabels', '--xsize'])
+print "----------------------------------"
+#load uclust into dictionary for LCA taxonomy adjustments
+print 'Determining Last Common Ancestor (LCA) for clustered seqs'
+LCA = {}
+with open(UC, 'rU') as input:
+    for line in input:
+        line = line.replace('\n', '')
+        cols = line.split('\t')
+        if cols[3] != '*':
+            centroid = cols[9].rsplit(';', 2)[0]
+            hit = cols[8].rsplit(';', 2)[0]
+            centroidTax = centroid.split('tax=')[-1]
+            centroidID = centroid.split('tax=')[0].rstrip()
+            hitTax = hit.split('tax=')[-1]
+            centroidLCA = centroidTax.split(',')
+            hitLCA = hitTax.split(',')
+            if not centroidLCA == hitLCA:
+                if len(centroidLCA) == len(hitLCA):
+                    #tax levels are the same, so now get LCA
+                    for x in range(0,len(centroidLCA)):
+                        if not centroidLCA[x] == hitLCA[x]:
+                            Tax = centroidLCA[:x]
+                else:
+                    #if not the same length, then determine if they are identical to same level of taxonomy
+                    if len(centroidLCA) > len(hitLCA):
+                        for x in range(0,len(hitLCA)):
+                            if not centroidLCA[x] == hitLCA[x]:
+                                Tax = centroidLCA[:x]
+                            else:
+                                Tax = centroidLCA
+                    else:
+                        for x in range(0,len(centroidLCA)):
+                            if not hitLCA[x] == centroidLCA[x]:
+                                Tax = hitLCA[:x]
+                            else:
+                                Tax = hitLCA
+                #generate name change dictionary called LCA
+                LCA[centroid] = '%stax=%s' % (centroidID, ','.join(Tax))
+print "----------------------------------"
+#ok, now load in the centroid fasta file, rename if needed, and filter based on duplicates
+#they have been clustered, so sequences with the same taxonomy down to species level need to be resolved
+#also for utax training, likely not worth keeping seqs only classified to class
+print "Renaming centroids and filtering data"
+TaxSeen = []
+UtaxOut = args.out+'.utax.fa'
+UsearchOut = args.out+'.usearch.fa'
+total = 0
+okayTax = 0
+duplicateSp = 0
+with open(UtaxOut, 'w') as utax:
+    with open(UsearchOut, 'w') as usearch:
+        with open(OTUs, 'rU') as input:
+            for rec in SeqIO.parse(input, 'fasta'):
+                #entire ID is in rec.description
+                if rec.description in LCA:
+                    ID = LCA.get(rec.description)
+                else:
+                    ID = rec.description
+                #for usearch out, we want all files, so output here
+                rec.id = ID
+                rec.name = ''
+                rec.description = ''
+                SeqIO.write(rec, usearch, 'fasta')
+                total += 1
+                tax = ID.split('tax=')[-1]
+                taxLevels = tax.count(',') #0 = kingdom, 1 = phylum, 2 = class, 3 = order, 4 = family, 5 = genus, 6 = species
+                if taxLevels > 2:
+                    okayTax += 1
+                    if taxLevels > 5:
+                        if not tax in TaxSeen:
+                            TaxSeen.append(tax)
+                        else:
+                            duplicateSp += 1
+                            continue  #skip if duplicated, just taking first one, hopefully this is okay...
+                    SeqIO.write(rec, utax, 'fasta')
+print "----------------------------------"
+print '%i input Seqs' % countfasta(args.input)
+print '%i Seqs after 97%% clustering' % total
+print '%i Seqs saved to %s' % (total, UsearchOut)
+print '%i Seqs removed low taxonomy' % (total - okayTax)
+print '%i Seqs removed duplicate species' % (duplicateSp)
+print '%i Seqs saved to %s' % (countfasta(UtaxOut), UtaxOut)
+print "----------------------------------"
+if not args.debug:
+    shutil.rmtree(tmpdir)
