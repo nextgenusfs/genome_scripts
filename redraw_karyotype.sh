bdf3f7f9de8d67d992c94107f0ccd00c5e6fd5a0
CFMR NGS <ngs@cfmrs-mac-pro.fpl.fs.fed.us>
2017-04-06 13:57:09 -0500

    update from mac pro

--- redraw_karyotype.sh
+++ redraw_karyotype.sh
@@ -9,6 +9,9 @@ fi
 isolate=$1
 cd $isolate
 
+#script to draw the SM clusters
+
+
 #just need to update the seqids to draw all chromosomes, layout is same as last time you drew
 cut -f1 Af293.bed | gsort | uniq | paste -d',' -s - > seqids
 cut -f1 ReMap.bed | gsort | uniq | paste -d',' -s - >> seqids
@@ -19,3 +22,14 @@ python -m jcvi.graphics.karyotype seqids layout
 
 #rename
 mv karyotype.pdf $isolate.all_contigs.pdf
+
+#Microsynteny
+#python -m jcvi.compara.synteny mcscan grape.bed grape.peach.lifted.anchors --iter=1 -o grape.peach.i1.blocks
+python -m jcvi.compara.synteny mcscan Af293.bed Af293.ReMap.lifted.anchors --iter=1 -o Af293.ReMap.i1.blocks
+python -m jcvi.compara.synteny mcscan ReMap.bed ReMap.CEA10.lifted.anchors --iter=1 -o ReMap.CEA10.i1.blocks
+
+#need to select region of interest
+gawk '/AFUA_3G02400/ {p=1}; p; /AFUA_3G02770/ {p=0}' Af293.ReMap.i1.blocks > Af293.blocks
+#gawk '/AFUB_045860/ {p=1}; p; /AFUB_045570/ {p=0}' ReMap.CEA10.i1.blocks
+gawk '/AFUC_03224/ {p=1}; p; /AFUC_03260/ {p=0}' ReMap.CEA10.i1.blocks > CEA10.blocks
+python -m jcvi.graphics.synteny blocks grape_peach.bed blocks.layout
